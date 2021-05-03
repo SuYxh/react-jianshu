9ebad27c2440aada6c74e15a528d4da1922dbf07
jarvis <xinhao.yang@pactera.com>
2021-05-03 15:48:57 +0800

    redux-thunk的作用

--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,7 @@
 import { createStore, applyMiddleware } from "redux";
 import reducer from "./reudcer";
 import thunk from "redux-thunk";
+// redux-thunk的作用: https://blog.csdn.net/qq_42339350/article/details/111599426
 const store = createStore(reducer, applyMiddleware(thunk));
 
 export default store;
